03b1c3378c56c1c12261805772710549bd1d63b0
Petter Måhlén <petter@spotify.com>
2021-10-11 20:27:50 +0200

    Add time per question to final feedback

--- src/Main.kt
+++ src/Main.kt
@@ -4,6 +4,8 @@ import java.time.Duration
 import java.time.Instant
 import kotlin.random.Random
 
+private val secondsPerQuestion = 5
+
 /**
  * TODO: document
  */
@@ -14,6 +16,7 @@ fun main() {
     var totalTime = Duration.ZERO
     val numberOfQuestions = 20
 
+    // Stora minus10
     for (i in 1..numberOfQuestions) {
         val left = Random.nextInt(10, 20)
         val right = Random.nextInt(left)
@@ -21,7 +24,7 @@ fun main() {
         val timeBefore = Instant.now()
 
         var correct = false
-
+2
         while (!correct) {
             print("Fråga: $i - vad är $left - $right? ")
             try {
@@ -47,9 +50,9 @@ fun main() {
     val timePerQuestion = totalTime.seconds / numberOfQuestions
 
     print("Bra jobbat, du svarade rätt på $numberOfQuestions frågor på ${totalTime.seconds} sekunder!\n")
-    if (timePerQuestion < 5) {
+    if (timePerQuestion < secondsPerQuestion) {
         print("Du är snabb nog att klara stora minus!")
     } else {
-        print("Du behöver öva mera!")
+        print("Du behöver öva mera! Det tog $timePerQuestion sekunder per fråga, det får ta $secondsPerQuestion sekunder")
     }
 }
